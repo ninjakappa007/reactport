a8bab3f20ad375c5a5cb3b958e27f73ec3e06084
Ashutosh <ashutoshnayak410@gmail.com>
2024-02-18 09:18:59 +0530

    added resume

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Main.css"
+import { GrAttachment } from "react-icons/gr";
+
 
 const Main = () => {
   return (
@@ -7,8 +9,16 @@ const Main = () => {
 
 		<div className="container lg:h-[550px] h-[400px] lg:w-[1200px] w-[300px] flex justify-center items-center relative overflow-hidden ">
       <div className="card lg:w-[400px] lg:h-[250px]  w-[200px] h-[300px]">
-        <h1 className="font-bold text-3xl">Hi</h1>
-        <p className="font-semibold text-2xl">Currenly working at amazon , learning Web Dev using MERN Stack</p>
+        <h1 className="font-bold text-3xl">Hi ..</h1>
+        <p className="font-semibold lg:text-2xl text-xl">
+        Currenly working at Amazon, Skilled in MERN Stack and Web Automation .</p>
+        <a href="/assets/Resume_Ashutosh_Nayak_QA.pdf" download="Ashutosh's Resume" target='_blank'>
+
+        <button type="button" className="bg-white text-slate-900 p-2 rounded-lg mt-1 hidden lg:block opacity-65 hover:scale-105 duration-300">Download Resume</button>
+
+        <button type="button" className="bg-white text-slate-900 p-2 rounded-lg mt-1 block lg:hidden opacity-65">Resume</button>
+
+        </a>
       </div>
       <div class="blob lg:w-[500px] lg:h-[500px] h-[100px] w-[100px]"></div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
 
   return (
     <div className='max-w-[1200px] h-full mx-auto flex justify-between p-5 bg-white'>
@@ -15,26 +9,6 @@ const Navbar = () => {
         <h1 className='text-2xl'>Ashutosh Nayak</h1>
       </div>
 
-      {/* Hamburger menu for smaller screens */}
-      <div className='lg:hidden'>
-        <button onClick={toggleMenu}>
-          <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'>
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2'
-              d='M4 6h16M4 12h16m-7 6h7'></path>
-          </svg>
-        </button>
-
-        {menuOpen && (
-          <div className='flex flex-col gap-3 text-xl'>
-            <button onClick={toggleMenu}><a href='#projects'>Projects</a></button>
-            <button onClick={toggleMenu}><a href='#techstack'>About</a></button>
-            <button onClick={toggleMenu}><a href='#contact'>Contact Me</a></button>
-          </div>
-        )}
-      </div>
-
-      {/* Regular menu for larger screens */}
       <div className='lg:flex gap-6 text-xl hidden'>
         <div>
           <button><a href='#projects'>Projects</a></button>
