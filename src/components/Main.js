99290a3682c64c5707b31e9e423035a7539681f5
Ashutosh <ashutoshnayak410@gmail.com>
2024-02-18 12:48:15 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ const Main = () => {
 		<div className="container lg:h-[550px] h-[400px] lg:w-[1200px] w-[300px] flex justify-center items-center relative overflow-hidden ">
       <div className="card lg:w-[400px] lg:h-[250px]  w-[200px] h-[300px]">
         <h1 className="font-bold text-3xl">Hi ..</h1>
-        <p className="font-semibold lg:text-2xl text-xl">
+        <p className="font-semibold lg:text-2xl text-sm">
         Currenly working at Amazon, Skilled in MERN Stack and Web Automation .</p>
         <a href="/assets/Resume_Ashutosh_Nayak_QA.pdf" download="Ashutosh's Resume" target='_blank'>
 
