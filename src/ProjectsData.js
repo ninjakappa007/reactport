a00e2b18908c225f80a2b7508638c9e315b180a9
Ashutosh <ashutoshnayak410@gmail.com>
2024-02-20 08:01:38 +0530

    added imagewall project

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -9,5 +9,11 @@ export const ProjectsData = [
     bio: "Built a web application that closely mimicked the design of the popular Indian payment gateway, Razorpay, using HTML & Tailwind CSS.",
     github: "https://github.com/ninjakappa007/Razorpay_clone.git",
     live: "https://razerpay.netlify.app/",
-    image: "assets/razorpay.png"}
+    image: "assets/razorpay.png"},
+
+    {name : "ImageWall" , 
+    bio: "A simple React project that utilizes React.js, Context API, and Supabase for authentication and database operations. This project allows users to log in using OTP mail and manage their images. Users can upload, delete, and view images that are stored securely in their individual folders.",
+    github: "https://github.com/ninjakappa007/image-storage.git",
+    live: "https://image-storage-rho.vercel.app/",
+    image: "/assets/imagewall.png"}
 ];
